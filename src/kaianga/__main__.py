4f57806a5b5922f1a821aec352aa9cbef9f59bc6
Shaun Alexander <shaun@sierraalpha.co.nz>
2020-09-06 01:56:31 +0000

    Pretty close to executing scripts automatically

--- src/kaianga/__main__.py
+++ src/kaianga/__main__.py
@@ -17,7 +17,12 @@ Note: This skeleton file can be safely removed if not needed!
 
 import click
 import logging
+import numbers
+import os
 import sys
+import subprocess
+import time
+import toml
 
 from kaianga import __version__
 
@@ -38,9 +43,27 @@ def setup_logging(loglevel):
     logging.basicConfig(level=getattr(logging, loglevel.upper()), stream=sys.stdout,
                         format=logformat, datefmt="%Y-%m-%d %H:%M:%S")
 
+def run_command(root, prompt, cmd):
+    command = "{}".format(cmd if not root
+               else "echo 'entering root' "
+               "&& su -c "
+               "'{}' -m root".format(cmd))
+    shell_setting = root or prompt
+    import pdb; pdb.set_trace()
+    subprocess.run(command, shell=shell_setting)
+
+def process_command(task_name, task, running, executed_groups):
+    # pre task work (is it a pull command?)
+    try:
+        for x in [v[0] for k,v in task.items() if k not in "priority pre-req".split()]:
+            run_command(x["root"], True, x["script-path"])
+    except:
+        import pdb; pdb.set_trace()
+
+
 @click.command()
 @click.option(
-    "-c", "--config", default="~/.config/kaianga/kaianga.conf", show_default=True,
+    "-c", "--conf_file", default="~/.config/kaianga/kaianga.conf", show_default=True,
     help="The location of your config file"
 )
 @click.option(
@@ -59,7 +82,7 @@ def setup_logging(loglevel):
     "-v", "--verbose", is_flag=True, default=False,
     help="Set the output to verbose"
 )
-def app(config, log_level, initial, user, verbose):
+def app(conf_file, log_level, initial, user, verbose):
     """Kaianga app, bootstraps and refreshes home on demand
     """
 
@@ -67,10 +90,56 @@ def app(config, log_level, initial, user, verbose):
         log_level = "info"
     setup_logging(log_level)
 
-    if config.startswith(r"~/"):
-        config = config.replace(r"~/", r"/home/{}/".format(user), 1)
+    if conf_file.startswith(r"~/"):
+        conf_file = conf_file.replace(r"~/", r"/home/{}/".format(user), 1)
     _logger.debug("reading config from %s for user %s %s mode:",
-                  config, user, "in initial" if initial else "not in initial")
+                  conf_file, user, "in initial" if initial else "not in initial")
+
+    user_config = toml.load(conf_file)
+
+    # build default attributes
+
+    # ordered_config = prioritise(user_config)
+    # figure out order
+    task_groups ={
+        "{}.{}".format(x,z):{
+            z:y[z],
+            "priority": y.get("priority", float("inf")),
+            "pre-req": y.get("pre-req", None),
+        }
+        for x,y in user_config.items()
+        for z in y.keys()
+        if z.lower() not in "priority pre-req".split()
+    }
+
+    sorted_tasks = sorted(list(task_groups.items()),
+                          key=lambda x: x[1]["priority"]
+    )
+    executed_groups = [None]
+    running_tasks = []
+
+    while len(sorted_tasks) > 0:
+        current_prioirity = sorted_tasks[0][1]["priority"]
+        tasks_started = False
+        for i in range(len(sorted_tasks)-1):
+            task_priority = sorted_tasks[i][1]["priority"]
+            task_pre_req = sorted_tasks[i][1]["pre-req"]
+            if task_priority <= current_prioirity and task_pre_req in executed_groups:
+
+                # Marks when done and blocks if need be
+                process_command(*sorted_tasks.pop(0), running_tasks, executed_groups)
+                tasks_started = True
+
+        if not tasks_started:
+            _logger.debug("waiting on tasks %s, completed %s",
+                          ", ".join(running_tasks), ", ".join(executed_groups[1:])
+                          if executed_groups > 1 else "None")
+            sleep(2)
+
+
+    # run process, if it's blocking then wait till end
+
+    import pdb; pdb.set_trace()
 
     _logger.info("script ends here")
 
